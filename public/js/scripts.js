6e099e0ec95b91afaed558729552cb3e42314b5c
haritr16 <s223295149@deakin.edu.au>
2024-03-26 22:25:23 +1100

    Initial commit

---
+++ public/js/scripts.js
@@ -0,0 +1,21 @@
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const element = document.querySelectorAll('.modal');
+    const instances = M.Modal.init(element);
+
+    // To Handle form submission
+    document.getElementById('myForm').addEventListener('submit', (e) => {
+        e.preventDefault(); 
+
+        const formData = new FormData(e.target);
+        const formObject = {};
+
+        formData.forEach((value, key) => {
+            formObject[key] = value;
+        });
+
+        console.log(formObject);
+        e.target.reset(); 
+    });
+});
+
