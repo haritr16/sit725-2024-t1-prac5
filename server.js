8acf790bc089b2e5496ead6dd338f9ce41be045a
haritr16 <s223295149@deakin.edu.au>
2024-04-17 21:41:02 +1000

    mvc update

--- server.js
+++ server.js
@@ -1,54 +1,30 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const formRoutes = require('./routes/formRoutes');
 
 const app = express();
 
-app.use(express.static(__dirname+'/public'))
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// connection to MongoDB
+// MongoDB Connection
 const mongoURI = 'mongodb+srv://s223295149:cMLGEplq4XoycbeA@cluster0.uus7rft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-//  a mongoose schema
-const Schema = mongoose.Schema;
-const FormSchema = new Schema({
-    name: String,
-    email: String,
-    car: String
-});
+// Middleware
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Define a scheam called form to store form data
-const Form = mongoose.model('Form', FormSchema);
-
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-// API to handle form submission
-app.post('/submit', (req, res) => {
-    const { name, email, car } = req.body;
-    const newForm = new Form({ name, email, car });
-    newForm.save()
-        .then(() => res.send('Form submitted successfully'))
-        .catch(err => console.log(err));
-});
+// Routes
+app.use('/', formRoutes);
 
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
-
 
 
 
